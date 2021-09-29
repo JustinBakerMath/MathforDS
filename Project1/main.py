bc57e8caa44270d0869354f4dbb28c123bba44d7
trentdegiovanni <trentdegiovanni@gmail.com>
2021-09-29 15:26:11 -0600

    proximalg first draft
    Need to find Lipschitz constant for given f(x)

--- Project1/main.py
+++ Project1/main.py
@@ -8,7 +8,20 @@ A = generator.A
 b = generator.b
 coefs = generator.coefs
 
-initial = np.random.random(len(coefs))*max(abs(coefs))
+def f(x, A=A, b= b):
+    #given function (differentiable part)
+    F = 0
+    for i in range(0,len(b)):
+        F = F+1/2/len(b)*np.log(1+np.exp(-b[i]*np.dot(A[i],x)))
+    return F
+
+def gradf(x, A=A, b= b):
+    #given function (differentiable part)
+    F = 0
+    for i in range(0,len(b)):
+        F = F-1/2/len(b)*np.exp(-b[i]*np.dot(A[i],x))*A[i]*b[i]\
+            /(1+np.exp(-b[i]*np.dot(A[i],x)))
+    return F
 
 
 def F(x,lam = 0.001, order = 1, A=A,b=b):
@@ -16,16 +29,16 @@ def F(x,lam = 0.001, order = 1, A=A,b=b):
     F = 0
     for i in range(0,len(b)):
         F = F+1/2/len(b)*np.log(1+np.exp(-b[i]*np.dot(A[i],x)))
-    F = F + np.linalg.norm(x, ord=order)
+    F = F + lam*np.linalg.norm(x, ord=order)
     return F
 
-def sgF(x,A=A,b=b):
+def sgF(x,lam = 0.001, A=A,b=b):
     #subgradient of given function
     F = 0
     for i in range(0,len(b)):
         F = F-1/2/len(b)*np.exp(-b[i]*np.dot(A[i],x))*A[i]*b[i]\
             /(1+np.exp(-b[i]*np.dot(A[i],x)))
-        F = F + (x<0)*(-1)+(x>0)*(1)+(x==0)*0
+        F = F + lam*((x<0)*(-1)+(x>0)*(1)+(x==0)*0)
     return F
 
 def optimal_step(t,x,coefs = coefs):
@@ -51,7 +64,7 @@ def plot(y):
     plt.ylabel("F(x^t)-F(x*)")
     plt.show()
 
-def apply_sub_grad(initial,step_size=optimal_step,sg=sgF, coefs=coefs):
+def apply_sub_grad(initial,step_size=optimal_step,sg=sgF, coefs=coefs, F = F):
     #Sub-gradient implementation
     #Inputs
         #initial: initial vec (x1)
@@ -70,13 +83,51 @@ def apply_sub_grad(initial,step_size=optimal_step,sg=sgF, coefs=coefs):
         y = x - eta*sg(x)
         i+=1
         errs.append(F(y)-F(coefs))
-        print(F(y)-F(coefs))
-        print(F(y)-F(coefs))
         if F(y)<F(x):
             sol = y
         x = y
         
     return errs,sol
 
-(errs,sol)  = apply_sub_grad(initial)
-plot(errs)
+def proxl1(x,lam=0.001):
+    #proximal gradient operator for l1norm
+    #inputs:
+        #x: argument to evaluate at (vector)
+        #lam: lambda parameter for soft-thresholding
+    #ouputs:
+        #\psi_st(x;\lam)
+     return (x+lam)*(x<=-lam)+(x-lam)*(x>=lam)
+
+def apply_prox_grad(initial, reg = proxl1, step_size = 1/2, grad = gradf, f =f ):
+    #proximal gradient implementation for composite functions
+    #inputs:
+        #initial: initial vec
+        #reg: regularization term for prox op
+        #step_size: step size
+        #grad: gradient of differentiable part
+        #f: differentiable part of composite function
+    #Ouputs
+        #sol: solution
+        #errs: F(xt)-F(x*)
+    errs = []
+    x = initial
+    sol = initial
+    i = 1
+    while abs(F(x)-F(coefs))>1e-4:
+        y = proxl1(x - step_size*grad(x))
+        i+=1
+        errs.append(F(y)-F(coefs))
+        print(F(y)-F(coefs))
+        if F(y)<F(x):
+            sol = y
+        x = y
+        
+    return errs,sol
+######################Applying all methods 
+initial = np.random.random(len(coefs))*max(abs(coefs))
+
+#(errs,sol)  = apply_sub_grad(initial, F = lambda x :F(x, lam=0.1), sgF = lambda x :sgF(x, lam=0.1))
+#plot(errs)
+
+(errsprox, solprox) = apply_prox_grad(initial)
+plot(errsprox)
